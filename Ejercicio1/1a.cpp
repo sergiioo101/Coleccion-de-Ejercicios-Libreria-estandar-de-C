222871ca73eed4ecaa4a40ca552610b99fd9a9d8
sergiioo101 <smartros@myuax.com>
2023-12-18 13:01:07 +0100

    ejercicio 1a

---
+++ Ejercicio1/1a.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    // Declarar un número negativo
+    int num = -10;
+
+    // Obtener el valor absoluto utilizando abs()
+    int abs_num = abs(num);
+
+    printf("El valor absoluto de %d es %d\n", num, abs_num);
+
+    return 0;
+}
