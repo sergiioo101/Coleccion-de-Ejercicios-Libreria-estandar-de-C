9cba3f71d3563b7a61223e128a1501c413622622
sergiioo101 <smartros@myuax.com>
2023-12-18 13:02:38 +0100

    ejercicio 1b

--- Ejercicio1/1b.cpp
+++ Ejercicio1/1b.cpp
@@ -1,3 +1,22 @@
-//
-// Created by PC on 18/12/2023.
-//
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+
+int main() {
+    // Semilla inicial basada en el tiempo actual
+    srand(time(0));
+
+    // Generar y mostrar el primer número aleatorio
+    int random_number = rand();
+    printf("Número aleatorio (semilla original): %d\n", random_number);
+
+    // Cambiar la semilla utilizando un valor específico
+    unsigned int nueva_semilla = 123;  // Puedes cambiar este valor
+    srand(nueva_semilla);
+
+    // Generar y mostrar un nuevo número aleatorio con la nueva semilla
+    random_number = rand();
+    printf("Número aleatorio (nueva semilla): %d\n", random_number);
+
+    return 0;
+}
